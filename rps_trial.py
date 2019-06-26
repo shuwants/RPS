dae3eac4b899e122c234253b3208773d93001b99
Shuji Kato <shuwants@gmail.com>
2019-06-26 20:10:04 +0900

    Modifying RandomPlayer

--- rps_trial.py
+++ rps_trial.py
@@ -11,8 +11,6 @@ class RandomPlayer(Player):
     def move(self):
         return random.choice(moves)
 
-PlayerA = RandomPlayer()
-PlayerB = RandomPlayer()
 
 def beats(one, two):
     return((one == "rock" and two == "scissors") or (one == "scissors" and two == "paper") or
@@ -24,8 +22,8 @@ class Game:
         self.p2 = p2
 
     def play_round(self):
-        move1 = self.p1.move(PlayerA)
-        move2 = self.p2.move(PlayerB)
+        move1 = self.p1.move()
+        move2 = self.p2.move()
         print(f"Player 1: {move1}) Player 2: {move2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
