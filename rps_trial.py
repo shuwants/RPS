5b3d066ddcca0c92c9b2ebb7f0853ed92b544857
Shuji Kato <shuwants@gmail.com>
2019-06-27 20:51:56 +0900

    Setting score.

--- rps_trial.py
+++ rps_trial.py
@@ -1,5 +1,6 @@
 import random
 moves = ['rock', 'paper', 'scissors']
+
 class Player:
     def move(self):
         return 'rock'
@@ -20,20 +21,36 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.p1.score = 0
+        self.p2.score = 0
+        self.ties = 0
 
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"Player 1: {move1}) Player 2: {move2}")
+        print(f"Player 1: {move1} Player 2: {move2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
+        if beats(move1, move2) == True:
+            print("** PLAYER 1 WINS **")
+            self.p1.score += 1
+            print(f"Score: Player 1 {self.p1.score}, Player 2 {self.p2.score}")
+        elif beats(move2, move1) == True:
+            print("** PLAYER 2 WINS **")
+            self.p2.score += 1
+            print(f"Score: Player 1 {self.p1.score}, Player 2 {self.p2.score}")
+        else:
+            print("** TIE **")
+            print(f"Score: Player 1 {self.p1.score}, Player 2 {self.p2.score}")
 
     def play_game(self):
         print("Game start!")
         for round in range(3):
             print(f"Round {round + 1}:")
             self.play_round()
+
         print("Game over!")
+
 
 if __name__ == "__main__":
     game = Game(RandomPlayer(), RandomPlayer())
