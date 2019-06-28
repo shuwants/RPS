cad0e88277aff81d0e138984cc6e327adbdc118d
Shuji Kato <shuwants@gmail.com>
2019-06-28 19:38:36 +0900

    Adding input function.

--- rps_trial.py
+++ rps_trial.py
@@ -1,6 +1,21 @@
 import random
 moves = ['rock', 'paper', 'scissors']
 
+def valid_input(prompt, options):
+    while True:
+        response = input(prompt).lower()
+        for option in options:
+            if option in response:
+                return response
+        print("Sorry, I don't understand.")
+
+def ask_input():
+    rps = valid_input("Rock, paper, scissors? ('q' for quit)> ", ["rock", "paper", "scissors", "q"])
+    if "q" in rps:
+        print("Thanks for playing! See you next time.\n")
+    else:
+        print("play_game()")
+
 class Player:
     def move(self):
         return 'rock'
@@ -14,7 +29,7 @@ class RandomPlayer(Player):
 
 class HumanPlayer(Player):
     def move(self):
-        return input("Rock, paper, scissors? > ")
+        return input("Rock, paper, scissors? > ").lower()
 
 
 def beats(one, two):
@@ -32,7 +47,7 @@ class Game:
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"Player 1: {move1} Player 2: {move2}")
+        print(f"You played {move1}\nOpponent played {move2}.")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
         if beats(move1, move2) == True:
